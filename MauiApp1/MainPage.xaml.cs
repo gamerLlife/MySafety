a131e02c0aff08f9e5b16671982650b661850ed7
User <User@LAPTOP-2NHG13VP>
2024-10-22 14:41:20 +0800

    Add project files.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnRegisterClicked(object sender, EventArgs e)
+        {
+            count = count == 1 ? 0 : 1;
+            btn_register.Text = count == 1 ? $"Registered" : "Register";
+            SemanticScreenReader.Announce(btn_register.Text);
+        }
+    }
+
+}
